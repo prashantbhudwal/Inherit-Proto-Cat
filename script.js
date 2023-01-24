7bee0c2b91c007d04616cf957a8dd6334631d0fd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-01-24 13:27:24 +0530

    Testing Prototype Chains

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 function Cat() {
-
+  this.legs = 4;
+  this.color = `green`;
+  //   this.ears = 5; //Takes preference
 }
 
-let misty = new Cat();
+// Constructor --> Proto
 
+// Adding properties
+Cat.prototype.ears = 2;
+
+console.log(`About Misty>>>>>>>>>>>>>>>>>>>>`);
+let misty = new Cat();
 console.log(misty);
-console.log(misty);
+console.log(misty.legs);
+console.log(misty.color);
+console.log(`Ears`, misty.ears);
+
+console.log(Cat.color);
+
+console.log(Cat.prototype.constructor.color);
+
+Cat.color = "blue";
+
+console.log(Cat.color);
+
+console.log(`About Bob>>>>>>>>>>>>>>>>>>>>>>>>`);
+let bob = new Cat();
+console.log(bob);
+console.log(bob.legs);
+console.log(bob.color);
+console.log(`Ears`, bob.ears);
+constructor.color;
