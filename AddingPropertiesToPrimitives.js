bc4797b6f9a7e0e1928653db82ed43bb1d6db750
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-01-25 09:26:43 +0530

    Adding properties to primitives (+ inheritance)

---
+++ AddingPropertiesToPrimitives.js
@@ -0,0 +1,50 @@
+export default function AddingPropertiesToPrimitives() {
+  let dogString = "dog eats meat";
+
+  console.log(dogString);
+
+  //This works because of primitive wrapper
+  console.log(dogString.length);
+
+  //This does not work because primitives are immutable
+  // dogString.testObject = { a: "this object" };
+
+  //This works because now the object is added to the
+  //primitive wrapper's prototype
+
+  dogString.__proto__.dogObject = {
+    whoCreated: "this object was created by dogString",
+    dogProperty: "this property added by dogString",
+  };
+
+  // Test object gets assigned to the prototype of the primitive wrapper of all
+  //Strings you create after this point. Let's test that.
+
+  //This is how you access the primitive wrapper
+  // You are basically using a wrapper constructor to create a string object.
+
+  let primitiveObject = new String("cat eats meat too!");
+
+  console.log(primitiveObject);
+
+  // Now if you check the output object,
+  //it's prototype has has a property dogObject
+
+  // This works because of prototype inheritance chain.
+  //Js checks if dogString has a dogObject property
+  //Else it goes to its prototype to check
+
+  console.log(dogString.dogObject.dogProperty);
+
+  // How inheritance works.
+
+  let lionString = "lions obviously eat meat!";
+  lionString.__proto__.dogObject.lionProperty =
+    "I am a lion property added by lionString on dogObject";
+
+  // This works
+  console.log(dogString.dogObject.lionProperty);
+
+  //The prototype traversal makes this possibel.
+  //In Js the prototype ensures inheritance.
+}
--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-function Cat() {
-  this.legs = 4;
-  this.color = `green`;
-  //   this.ears = 5; //Takes preference
-}
+import AddingPropertiesToPrimitives from "./AddingPropertiesToPrimitives.js";
 
-// Constructor --> Proto
-
-// Adding properties
-Cat.prototype.ears = 2;
-
-let misty = new Cat();
-console.log(misty);
-console.log(misty.legs);
-console.log(misty.color);
-console.log(`Ears`, misty.ears);
-
-console.log(Cat.color);
-console.log(Cat.prototype.constructor.color);
-
-Cat.color = "blue";
-console.log(Cat.color);
-
-let bob = new Cat();
-console.log(bob);
-console.log(bob.legs);
-console.log(bob.color);
-console.log(`Ears`, bob.ears);
-constructor.color;
+AddingPropertiesToPrimitives();
