56e1a244994267d2f8bd13aa4a269fd6efef5926
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-12 08:25:49 +0530

    LO: async-await is syntactical sugar for fetch

--- asyncJs/asyncAwait.js
+++ asyncJs/asyncAwait.js
@@ -9,20 +9,17 @@ function asyncAwait() {
     "triad",
     "quad",
   ];
-
   const callParameters = {
     format: "hex",
     mode: modes[1],
     numberOfColors: "6",
     seed: "283848",
   };
-
   const { format, mode, numberOfColors, seed } = callParameters;
-
   const baseUrl = `https://www.thecolorapi.com`;
   const endpoint = `/scheme?${format}=${seed}&mode=${mode}&count=${numberOfColors}`;
 
-  console.log(baseUrl + endpoint);
+  /** Fetching and array of colors using then, catch */
   fetch(baseUrl + endpoint)
     .then((response) => response.json())
     .then((data) => {
@@ -30,6 +27,17 @@ function asyncAwait() {
       const colorCodes = colorArray.map((color) => color.hex.value);
       console.log(colorCodes);
     });
+
+  /** Equivalent code with Async Await*/
+  const fetcher = async function () {
+    // await can only be written inside async functions
+    const response = await fetch(baseUrl + endpoint);
+    const data = await response.json();
+    const colorArray = data.colors;
+    const colorCodes = colorArray.map((color) => color.hex.value);
+    return colorCodes;
+  };
+  fetcher().then((result) => console.log(result)); // async functions return a promise that needs to be resolved before use.
 }
 
 asyncAwait();
